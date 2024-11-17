3b7eba3bdf526f90a869501a5848fa4512e053c1
Hilmi Tolga SAHIN <htolgasahin@gmail.com>
2024-11-17 15:32:03 +0100

    feat: added convenience script and completions adding script

--- src/install_completions.sh
+++ src/install_completions.sh
@@ -3,6 +3,9 @@ COMPLETIONS_PATH="$HOME/.creator-completions.zsh"
 deno run -A main.ts completions zsh --name creator > $COMPLETIONS_PATH
 
 echo "Add this to your $HOME/.zshrc file:"
+echo "================================================"
 echo "source $COMPLETIONS_PATH"
- 
+echo "================================================"
+
+echo ""
 echo "Also make sure \"creator\" is installed and is in your PATH."
